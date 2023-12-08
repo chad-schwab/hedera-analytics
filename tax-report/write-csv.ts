b92bc5d91e1342134053b54254b1a250882e5b2f
Chad Schwab <chad.schwab@lworks.io>
2023-12-07 20:36:02 -0600

    extract meta csv writing

--- tax-report/write-csv.ts
+++ tax-report/write-csv.ts
@@ -11,12 +11,14 @@ const logger = createLogger("csv");
 type ColumnTokenStrategy = { strategy: "column"; allTokens: { tokenId: string; tokenSymbol: string }[]; targetTokenId?: string };
 export type TokenStrategy = { strategy: "omit" } | { strategy: "join" } | ColumnTokenStrategy;
 export type NftStrategy = { strategy: "omit" } | { strategy: "include" };
+export type MetaStrategy = { strategy: "omit" } | { strategy: "include" };
 export type StakingRewardStrategy = { strategy: "omit" } | { strategy: "include" };
 
 type CsvOptions = Partial<{
   tokenStrategy: TokenStrategy;
   nftStrategy: NftStrategy;
   stakingStrategy: StakingRewardStrategy;
+  metaStrategy: MetaStrategy;
 }>;
 
 function handleColumnStrategy(tokenStrategy: ColumnTokenStrategy, tokenTransfers: LoadedTokenTransfer[]) {
@@ -33,6 +35,12 @@ function handleColumnStrategy(tokenStrategy: ColumnTokenStrategy, tokenTransfers
       [`${token.tokenSymbol}:${token.tokenId} G/L`]: tokenTransfers.find((t) => t.tokenId === token.tokenId)?.decimalAmount,
     };
   }, {});
+}
+
+function writeStakingRewardColumns(stakingStrategy: StakingRewardStrategy, i: LoadedTransaction) {
+  return stakingStrategy.strategy === "omit"
+    ? undefined
+    : { "Hbar Staking Reward": i.stakingReward, "Staking Reward USD": i.stakingReward * i.exchangeRate };
 }
 
 function writeFungibleTokenColumns(tokenStrategy: TokenStrategy, tokenTransfers: LoadedTokenTransfer[]) {
@@ -54,11 +62,40 @@ function writeFungibleTokenColumns(tokenStrategy: TokenStrategy, tokenTransfers:
   }
 }
 
+function writeNftColumns(nftStrategy: NftStrategy, i: LoadedTransaction, accountId: string) {
+  return nftStrategy.strategy === "omit"
+    ? undefined
+    : {
+        "NFT Token ID": i.nftTransfer?.tokenId || i._attributedNft?.split(":")?.at(0),
+        Serial: i.nftTransfer?.serialNumber || i._attributedNft?.split(":")?.at(1),
+        NFT: i.nftTransfer ? `${i.nftTransfer.tokenId}:${i.nftTransfer.serialNumber}` : i._attributedNft,
+        "NFT Name": i.nftTransfer?.tokenName,
+        "NFT Symbol": i.nftTransfer?.tokenSymbol,
+        "NFT Sold?": i.nftTransfer?.senderAccount === accountId,
+      };
+}
+
+function writeMetaColumns(metaStrategy: MetaStrategy, i: LoadedTransaction) {
+  return metaStrategy.strategy === "omit"
+    ? undefined
+    : {
+        multiTokenTransaction: i._splitNfts || Number(Boolean(i.nftTransfer)) + i.tokenTransfers.length > 1,
+        exchangeRate: i.exchangeRate,
+        aggregated: i._aggregated ?? false,
+        splitNfts: i._splitNfts ?? false,
+      };
+}
+
 export function writeCsv(
   accountId: string,
   transactions: LoadedTransaction[],
   fileName: string,
-  { tokenStrategy = { strategy: "join" }, nftStrategy = { strategy: "include" }, stakingStrategy = { strategy: "include" } }: CsvOptions = {}
+  {
+    tokenStrategy = { strategy: "join" },
+    nftStrategy = { strategy: "include" },
+    stakingStrategy = { strategy: "include" },
+    metaStrategy = { strategy: "omit" },
+  }: CsvOptions = {}
 ): Promise<void> {
   const transformedItems = transactions.map((i) => {
     const { transactionId } = i;
@@ -66,38 +103,24 @@ export function writeCsv(
     return {
       Year: i.timestamp.getFullYear(),
       Date: i.timestamp.toLocaleString().replaceAll(",", ""),
-      Memo: i.memo,
+      Memo: i.memo.replaceAll("\n", ""),
       "Hbar G/L": i.hbarTransfer,
       "Sales Proceed": usd > 0 ? usd : 0,
       "Cost Basis": usd < 0 ? usd : 0,
       "G/L": usd,
-      ...(stakingStrategy.strategy === "omit"
-        ? undefined
-        : { "Hbar Staking Reward": i.stakingReward, "Staking Reward USD": i.stakingReward * i.exchangeRate }),
+      ...writeStakingRewardColumns(stakingStrategy, i),
       "Hbar From Accounts": i.hbarFromAccount.join(","),
       "Hbar To Accounts": i.hbarToAccount.join(","),
       ...writeFungibleTokenColumns(tokenStrategy, i.tokenTransfers),
-      ...(nftStrategy.strategy === "omit"
-        ? undefined
-        : {
-            "NFT Token ID": i.nftTransfer?.tokenId || i._attributedNft?.split(":")?.at(0),
-            Serial: i.nftTransfer?.serialNumber || i._attributedNft?.split(":")?.at(1),
-            NFT: `${i.nftTransfer?.tokenId}:${i.nftTransfer?.serialNumber}` || i._attributedNft,
-            "NFT Name": i.nftTransfer?.tokenName,
-            "NFT Symbol": i.nftTransfer?.tokenSymbol,
-            "NFT Sold?": i.nftTransfer?.senderAccount === accountId,
-          }),
+      ...writeNftColumns(nftStrategy, i, accountId),
       explore: `https://explore.lworks.io/mainnet/transactions/${transactionId}`,
-      multiTokenTransaction: i._splitNfts || Number(Boolean(i.nftTransfer)) + i.tokenTransfers.length > 1,
-      exchangeRate: i.exchangeRate,
-      aggregated: i._aggregated ?? false,
-      splitNfts: i._splitNfts ?? false,
+      ...writeMetaColumns(metaStrategy, i),
       UTC: i.timestamp.toISOString(),
     };
   });
 
   return new Promise((res, rej) => {
-    stringify(transformedItems, { header: true }, (e, output) => {
+    stringify(transformedItems, { header: true, cast: { boolean: (v: boolean) => String(v) } }, (e, output) => {
       if (e) {
         console.error("Error writing CSV: ", fileName);
         rej(e);
