5936936b329190bf012bb3c9caf151b99a5e0f94
Chad Schwab <chad.schwab@lworks.io>
2023-12-29 23:48:13 -0600

    output is overwritten per day to allow multiple copies of tax report

--- tax-report/write-transaction-report/prepare-out-directory.ts
+++ tax-report/write-transaction-report/prepare-out-directory.ts
@@ -1,10 +1,10 @@
-import { mkdir } from "node:fs/promises";
+import { mkdir, rmdir } from "node:fs/promises";
 import path from "node:path";
 
 const baseDir = path.join(__dirname, "..", "output", "tax-report");
 
 export function getOutputBase(year: number, account: string): string {
-  return path.join(baseDir, year.toString(), account);
+  return path.join(baseDir, year.toString(), account, new Date().toISOString().slice(0, 10));
 }
 
 export async function prepareOutDirectories(year: number, account: string) {
@@ -13,9 +13,10 @@ export async function prepareOutDirectories(year: number, account: string) {
   const soldTokensDir = path.join(outputDir, "sold-tokens");
   const soldNftsDir = path.join(outputDir, "sold-nfts");
 
-  await mkdir(outputDir, { recursive: true }).catch(() => {});
-  await mkdir(allTimeDir).catch(() => {});
-  await mkdir(soldTokensDir).catch(() => {});
-  await mkdir(soldNftsDir).catch(() => {});
+  await rmdir(outputDir, { recursive: true }).catch(() => {});
+  await mkdir(outputDir, { recursive: true });
+  await mkdir(allTimeDir);
+  await mkdir(soldTokensDir);
+  await mkdir(soldNftsDir);
   return { outputDir, allTimeDir, soldTokensDir, soldNftsDir };
 }
